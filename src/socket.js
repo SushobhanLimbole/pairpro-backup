5c3a1f20c8e925ee7246cf98a8c1a74af5c04a99
SushobhanLimbole <sushobhanlimbole17@gmail.com>
2025-06-20 10:54:28 +0530

    chat version 1.5

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://pairpro-backend-backup.onrender.com", {
   transports: ["websocket"],
   autoConnect: true, // Auto connect once globally
   reconnectionAttempts: 5,
